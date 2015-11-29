0cb9a69b5825c8b70e8d49383b62379446ae66e2
LeroyJeremy <bandit.mikado@hotmail.fr>
2015-11-29 20:09:35 +0100

    Modification readme+ suppression du code en commentaire

--- README_EMOT.md
+++ README_EMOT.md
@@ -1,5 +1,5 @@
----------------Instruction pour la partie émoticone personnalisé--------------------
+---------------Instruction pour la partie émoticone personnalisée--------------------\n
 
--1- Dans la console il faut faire sudo npm install dans le repertoire public/angular-smilies pour récuperer les modules nécessaires.
--2- Après l'upload d'un ou des émoticones pour les ajoutés dans la liste des émoticones pour les clients, il faut faire grunt dans la console dans le repertoire public/angular-smilies où le gruntfile.js se trouve.
--3- Il est nécessaire de faire un rafraîchissement de la page pour que les emoticones soit utilisable par les clients.
+-1- Dans la console il faut faire sudo npm install dans le repertoire filesync/public/angular-smilies pour récuperer les modules nécessaires et sudo npm install -g grunt-cli si la commande grunt n'es toujours pas accessible dans la console.\n
+-2- Après l'upload d'un ou des émoticone(s) pour les ajouter dans la liste des émoticones pour les clients, il faut faire grunt dans la console dans le répertoire filesync/public/angular-smilies où le gruntfile.js se trouve.\n
+-3- Il est nécessaire de faire un rafraîchissement de la page pour que les émoticones soit utilisable par les clients.\n
--- public/app/SocialCtrl.js
+++ public/app/SocialCtrl.js
@@ -8,9 +8,6 @@ angular
     this.message = '';
     this.fileUploadMsg = '';
 
-    //var arr = this.arr = [{"text": ":fuck", "img": "components/angular-smilies-master/src/smilies/nouveauSmile/fuk.gif"}];
-
-
     this.sendMessage = function() {
       console.log(this.message);
       SocketIOService.sendMessage(this.message);
@@ -23,40 +20,7 @@ angular
      $scope.$apply();
 
  };
-/*
- this.choixEmote = function(icone){
-    console.log("ok");
-    document.getElementById("tchatText").value += icone.text;
-    console.log(document.getElementById("tchatText").value);
- };
-  */
-/*
-  $(function () {//faire une directive
-      $('#uploadForm').on('submit', function (e) {
-          // On empêche le navigateur de soumettre le formulaire
-          e.preventDefault();
-          var $form = $(this);
-          var formdata = (window.FormData) ? new FormData($form[0]) : null;
-          var data = (formdata !== null) ? formdata : $form.serialize();
 
-          $.ajax({
-              url: $form.attr('action'),
-              type: $form.attr('method'),
-              contentType: false, // obligatoire pour de l'upload
-              processData: false, // obligatoire pour de l'upload
-              data: new FormData(this),
-              xhr: function() {
-              myXhr = $.ajaxSettings.xhr();
-              return myXhr;
-          },
-          success: function(res) {
-          	this.fileUploadMsg = res;
-            $scope.$apply();
-          },
-          });
-      });
-  });
-*/
  SocketIOService.onMessagesUpdated(onMessagesUpdated.bind(this));
 
     function onViewersUpdated(viewers) {
